046cc1a8265eebdea43dd7f500c72f3bb80ad082
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-06 23:43:28 -0800

    Readme added

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const express = require('express');
 //const res = require('express/lib/response');
 const app = express();
+app.use('/places', require('./controllers/places'));
 app.get('/', (req, res) => {
     res.send('Stuff');
 });
+
 //Adding wildcard route
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
-})
+});
+
+//Connecting to server
 app.listen(process.env.PORT);
