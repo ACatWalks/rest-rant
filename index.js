c9c1b3e9172b7500e3ae7b0d68b36eaa5dc8f93f
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-08 12:59:00 -0800

    Adding home page view

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //Basic setup
 require('dotenv').config();
 const express = require('express');
+//My computer must have auto-added this line when I forgot to include req and res as params for my callback function because I did not write the following line
 //const res = require('express/lib/response');
 const app = express();
+
+//Middleware
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 });
 
 //Adding wildcard route
