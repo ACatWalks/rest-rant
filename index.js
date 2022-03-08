6aaf01f6fe2afef0583688e2016ad298967a48b8
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-08 13:04:18 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 //Adding wildcard route
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404');
 });
 
 //Connecting to server
