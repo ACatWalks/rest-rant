4fcee36225968c97586530072d186eb43c9833ec
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-08 13:29:55 -0800

    new, show, and edit views stubbed out

---
+++ views/new.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Def = require('./default');
+
+function newRant() {
+    return (
+        <Def>
+            <main>
+                <h1>Start a New Rant or Rave</h1>
+            </main>
+        </Def>
+    )
+}
+
+modules.exports = newRant
---
+++ views/show.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Def = require('./default');
+
+function show() {
+    return (
+        <Def>
+            <main>
+                <h1>This Place's Rants and Raves</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = show
