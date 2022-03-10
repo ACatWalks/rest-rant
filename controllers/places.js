ea3dfe3c39d239672b7873f703f3b13d251471d5
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-09 20:32:02 -0800

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -6,14 +6,14 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/bon-vivant-qom5MPOER-I-unsplash.jpg'
     }, 
     {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/nathan-dumlao-zUNs99PGDg0-unsplash.jpg'
     }];
     res.render('places/index', { places });
 })
--- views/home.jsx
+++ views/home.jsx
@@ -5,8 +5,14 @@ function home(){
     return (
         <Def>
             <main>
-                <h1>Home</h1>
-                <button className="btn-primary">Places Page</button>
+                <h1>REST-Rant</h1>
+                <div>
+                <img src="/images/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg" alt="toast with bananas and blueberries"></img>
+                <div>
+                    Photo by <a href="https://unsplash.com/@miracletwentyone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joseph Gonzalez</a> on <a href="https://unsplash.com/photos/zcUgjyqEwe8">Unsplash</a>
+                </div>
+                </div>
+                <a href="/places"><button className="btn-primary">Places Page</button></a>
             </main>
         </Def>
     )
