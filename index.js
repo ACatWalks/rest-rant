f21c3701814bb9e90347bbe3fb4610ae23747d3c
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-12 13:06:11 -0800

    Delete functionality implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Basic setup
 require('dotenv').config();
 const express = require('express');
+const methodOverride = require('method-override');
 //My computer must have auto-added this line when I forgot to include req and res as params for my callback function because I did not write the following line
 //const res = require('express/lib/response');
 const app = express();
@@ -10,6 +11,7 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.use('/places', require('./controllers/places'));
 
