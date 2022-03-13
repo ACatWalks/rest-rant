792d0dbf5aac7b60a45b0d63212f78461ea80aaa
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-13 16:23:55 -0700

    Added edit form

--- controllers/places.js
+++ controllers/places.js
@@ -23,6 +23,40 @@ router.post('/', (req, res) => {
     res.redirect('/places');
 })
 
+//EDIT places
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id);
+    if(isNaN(id)){
+        res.render('error404');
+    } else if(!places[id]){
+        res.render('error404');
+    } else{
+        res.render('places/edit', { place: places[id] });
+    }
+})
+
+//PUT places
+router.put('/:id/edit', (req, res) => {
+    let id = Number(req.params.id);
+    if(isNaN(id)){
+        res.render('error404');
+    } else if(!places[id]){
+        res.render('error404');
+    } else{
+        if(!req.body.pic){
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if(!req.body.city){
+            req.body.city = 'Anytown'
+        }
+        if(!req.body.state){
+            req.body.state = 'USA'
+        }
+        places[id] = req.body;
+        res.redirect(`/places/${id}`);
+    }
+})
+
 //GET /places
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id);
@@ -34,7 +68,7 @@ router.get('/:id', (req, res) => {
         res.render('places/show', {place: places[id], id});
     }
 })
-//EDIT places
+
 //DELETE places
 router.delete('/places/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -54,4 +88,5 @@ router.delete('/places/:id', (req, res) => {
 router.get('/', (req, res) => {
     res.render('places/index', { places });
 })
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //Basic setup
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const methodOverride = require('method-override');
 //My computer must have auto-added this line when I forgot to include req and res as params for my callback function because I did not write the following line
 //const res = require('express/lib/response');
-const app = express();
+
 
 //Middleware
+//app.set('views', _dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'));
