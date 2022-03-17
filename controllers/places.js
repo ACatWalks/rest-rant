5c14f0a4c04335b01d5ade48e4c502a3e37993d3
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-17 11:45:00 -0700

    get and post routes now using mongoose, bonus completed

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const places = require('../models/places.js')
+const db = require('../models');
+const places = require('../models/places.js');
 
 //New route
 router.get('/new', (req, res) => {
@@ -9,34 +10,28 @@ router.get('/new', (req, res) => {
 //POST /places
 router.post('/', (req, res) => {
     console.log(req.body);
-    if(!req.body.pic){
-        //Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if(!req.body.city){
-        req.body.city = 'Anytown'
-    }
-    if(!req.body.state){
-        req.body.state = 'USA'
-    }
-    places.push(req.body);
-    res.redirect('/places');
+    db.Place.create(req.body).then(() => {
+        res.redirect('/places');
+    }).catch(err => {
+        console.log('err', err);
+        res.render('error404');
+    });
 })
 
 //EDIT places
-router.get('/:id/edit', (req, res) => {
+/*router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id);
     if(isNaN(id)){
         res.render('error404');
     } else if(!places[id]){
         res.render('error404');
     } else{
-        res.render('places/edit', { place: places[id] });
+        res.render('places/edit', { place: places[id], id });
     }
-})
+})*/
 
 //PUT places
-router.put('/:id/edit', (req, res) => {
+/*router.put('/:id/edit', (req, res) => {
     let id = Number(req.params.id);
     if(isNaN(id)){
         res.render('error404');
@@ -55,22 +50,20 @@ router.put('/:id/edit', (req, res) => {
         places[id] = req.body;
         res.redirect(`/places/${id}`);
     }
-})
+})*/
 
 //GET /places
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id);
-    if(isNaN(id)){
+    db.Place.findById(req.params.id).then(place => {
+        res.render('places/show', { place })
+    }).catch(err => {
+        console.log('err', err);
         res.render('error404');
-    } else if(!places[id]){
-        res.render('error404');
-    } else{
-        res.render('places/show', {place: places[id], id});
-    }
+    })
 })
 
 //DELETE places
-router.delete('/places/:id', (req, res) => {
+/*router.delete('/places/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
       res.render('error404')
@@ -82,11 +75,16 @@ router.delete('/places/:id', (req, res) => {
       places.splice(id, 1)
       res.redirect('/places')
     }
-  })
+  })*/
 
 //HOME route
 router.get('/', (req, res) => {
-    res.render('places/index', { places });
+    db.Place.find().then(places => {
+        res.render('places/index', { places })
+    }).catch(err => {
+        console.log(err);
+        res.render('error404');
+    })
 })
 
 module.exports = router;
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,32 +1,36 @@
 const React = require('react');
 const Def = require('../default');
 
-function edit_form(data){
+function EditForm({ place, id }){
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${id}/edit?_method=PUT`}>
                     <div className='row'>
-                    <div className='form-group col-sm-6'>
+                    <div className='form-group col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor='name'>Place Name</label>
-                        <input className="form-control" name="name" id="name" defaultValue={data.place.name} required />
+                        <input className="form-control" name="name" id="name" defaultValue={place.name} required />
                     </div>
-                    <div className='form-group col-sm-6'>
+                    <div className='form-group col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor='pic'>Place Picture</label>
                         <input className='form-control' id="pic" name="pic" />
                     </div>
-                    <div className='form-group col-sm-6'>
+                    <div className='form-group col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor='city'>City</label>
                         <input className='form-control' id="city" name="city" />
                     </div>
-                    <div className='form-group col-sm-6'>
+                    <div className='form-group col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor='state'>State</label>
                         <input className='form-control' id="state" name="state" />
                     </div>
-                    <div className='form-group col-sm-6'>
+                    <div className='form-group col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor='cuisines'>Cuisines</label>
-                        <input className='form-control' id="cuisines" name="cusines" defaultValue={data.place.cuisines} required />
+                        <input className='form-control' id="cuisines" name="cuisines" defaultValue={place.cuisines} required />
+                    </div>
+                    <div className='form-group col-sm-6 col-md-4 col-lg-3'>
+                        <label htmlFor='founded'>Founded In</label>
+                        <input className='founded' id='founded' name='founded' />
                     </div>
                     <input className='btn btn-primary' type="submit" value="Add Place" />
                     </div>
@@ -36,4 +40,4 @@ function edit_form(data){
     )
 }
 
-module.exports = edit_form
+module.exports = EditForm
